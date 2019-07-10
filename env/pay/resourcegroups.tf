b02c771c40c2dabadae8d9a2b5dcb9712d4ade5d
Simon Metcalf <simon_metcalf@hotmail.com>
2019-07-10 11:32:28 +0100

    group name

---
+++ env/pay/resourcegroups.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-test-rg"
+  location = "australiaeast"
+}
+
--- env/pay/service-umb-pay.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "pay" {
-  name     = "service-umb-pay"
-  location = "australiaeast"
-}
-
